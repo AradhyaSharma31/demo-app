a6c914da2ba2d4429d8a60801f898ae8d5b1ff14
AradhyaSharma31 <aradhyasharmaa96@gmail.com>
2026-02-02 11:32:32 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/Health.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Health {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return """
+                    Api was called,
+                    Server is healthy and running.
+                """;
+    }
+}
